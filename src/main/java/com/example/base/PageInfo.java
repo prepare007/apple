fcc9d8ff850b2d80e463b53289c4ca7865d5d679
liangwenshuo <2374994546@qq.com>
2019-03-12 12:00:08 +0800

    add(添加分页控制类)

---
+++ src/main/java/com/example/base/PageInfo.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) KYE 2019, All Rights Reserved
+ */
+
+package com.example.base;
+
+/**
+ * Routing
+ * Desc    TODO
+ * Source
+ * Created by LWS on 2019/3/5 15:00
+ * Version 1.0
+ */
+
+public class PageInfo {
+
+    private int pageNum;
+    private int pageSize;
+
+    public int getPageNum() {
+
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+
+        this.pageSize = pageSize;
+    }
+}
--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,61 +1,68 @@
 package com.example.controller;
 
-
 import com.example.base.BaseOpenApiResult;
+import com.example.base.PageInfo;
 import com.example.domain.User;
 import com.example.services.UserService;
 import com.example.util.APIResponseUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
-
 /**
  * Created by Administrator on 2017/8/16.
  */
 @Controller
-@RequestMapping(value = "/user")
+@CrossOrigin
+@RequestMapping (value = "/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @ResponseBody
-    @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"}, method =
+    @RequestMapping (value = "/add", produces = {"application/json;charset=UTF-8"}, method =
             RequestMethod.POST)
-    public BaseOpenApiResult addUser(User user){
+    public BaseOpenApiResult addUser(User user) {
+
         userService.addUser(user);
         return APIResponseUtil.makeOKRsp();
     }
 
     @ResponseBody
-    @RequestMapping(value = "/findUser/{id}", method = RequestMethod.GET)
-    public BaseOpenApiResult findOneUser(@PathVariable("id") Long id) {
+    @RequestMapping (value = "/findUser/{id}", method = RequestMethod.GET)
+    public BaseOpenApiResult findOneUser(@PathVariable ("id") Long id) {
+
         return APIResponseUtil.makeOKRsp(userService.findUserById(id));
     }
 
-
     @ResponseBody
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @RequestMapping (value = "/update", method = RequestMethod.POST)
     public long modifyUser(User user) {
+
         return userService.updateUser(user);
     }
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    public void modifyUser(@PathVariable("id") Long id) {
+
+    @RequestMapping (value = "/user/{id}", method = RequestMethod.DELETE)
+    public void modifyUser(@PathVariable ("id") Long id) {
+
         userService.deleteUser(id);
     }
 
     @ResponseBody
-    @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
-    public BaseOpenApiResult findAllUser(@PathVariable("pageNum") int pageNum,
-                                               @PathVariable("pageSize") int pageSize){
+    @RequestMapping (value = "/all",
+                     produces = {"application/json;charset=UTF-8"},
+                     method = RequestMethod.POST)
+    @CrossOrigin
+    public BaseOpenApiResult findAllUser(PageInfo pageInfo) {
 
-        return APIResponseUtil.makeOKRsp(userService.findAllUser(pageNum,pageSize));
+        return APIResponseUtil.makeOKRsp(userService.findAllUser(pageInfo.getPageNum(),
+                                                                 pageInfo.getPageSize()));
     }
 }
 
